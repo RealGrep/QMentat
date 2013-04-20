b55b6d91ec74660434eab34751f88012dbd0891e
Michel Dusseault <mike.dusseault@gmail.com>
2013-04-20 02:42:31 -0400

    Removed the .deb packaging files, which I've decided should go elsewhere and not be part of this main package. Currently keeping the package.sh file and the instructions in the INSTALL file, but those should be fixed/removed.

--- package.sh
+++ package.sh
@@ -2,7 +2,8 @@
 set -e
 
 #debuild -us -uc
-sudo debuild -i -us -uc -rsudo
-#debuild -S -us -uc
+#sudo debuild -i -us -uc -rsudo
+#sudo debuild -I -us -uc -rsudo
+debuild -i -us -uc -rfakeroot
 #su -c pbuilder build ../qmentat_0.8.dsc
 
